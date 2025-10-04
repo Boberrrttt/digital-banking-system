ec8092354f594d548702840e4403bdf4d829b13c
Robert Salvo <salvo.robertroy@gmail.com>
2025-10-04 20:13:22 +0800

    refactor: changed project structure

---
+++ main.c
@@ -0,0 +1,13 @@
+int main() {
+
+  printf("Digital Banking System starting...\n");
+
+  // run frontend
+  
+  
+  // run backend
+  
+  printf("Digital Banking System closing...\n");
+
+  return 0;
+}
