54f1167308b48e31588e073bb3c1b448fc7ce441
Robert Salvo <salvo.robertroy@gmail.com>
2025-10-07 22:14:05 +0800

    chore: edited account functionality

--- backend/account/account.h
+++ backend/account/account.h
@@ -1,11 +1,10 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
-void showMenu(const char* accountId);
 double getBalance(const char* accountId);
-void getProfileDetails(const char* userid);
+void getProfileDetails(const char* userId);
 void logout();
-void createUser(const char* name, const char* fingerprintId);
+void createUser(const char* name, const char* fingerprintId, double balance);
 int verifyPin(const char* pin);
 
 
